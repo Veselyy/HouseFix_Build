4eac41fb6d33fae710d1b154debe18b049bc5560
MartinVesely <20s.veselyma@seznam.cz>
2022-11-06 11:00:35 +0100

    Přidání validace formulářů

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
   new WOW().init();
 
-  
+// Modální okno
 var button=$('#button')
 var modal=$('#modal')
 var close=$('#close')
@@ -13,6 +13,29 @@ var close=$('#close')
     close.click(function(){
         modal.removeClass('modal-active');
     })
+    
+// Validace formuláře
+$('#brief-form').validate({
+  rules: {
+    // Musí být vyplnění
+    username: "required",
+    email: {
+      required: true,
+      // Musí být vyplněný stylem pro email
+      email: true
+    }
+  },
+  // Zprávy, které se zobrazí při erroru
+  // Zprávy se přepíšou podle nás
+    messages:{
+      username: "Prosím vyplňte pole!",
+      email:{
+        required: "Prosím vyplňte pole!",
+        email: "Správně zformátujte text!"
+      }
+    }
+});
+
 
 $('.slider').slick({
     slidesToShow: 3,
